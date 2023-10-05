b18f2134092d645732e4ec70d932499504979a82
Elijahtab <elijah.tabachnik@gmail.com>
2023-10-05 14:14:27 -0700

    Added Start Menu

--- Assets/Scripts/StatsManagerScript.cs
+++ Assets/Scripts/StatsManagerScript.cs
@@ -1,20 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class StatsManagerScript : MonoBehaviour
 {
     int points = 0;
-    int lives = 10;
+    int money = 200;
+    public int lives = 10;
+
+    public TMP_Text moneyText;
+    public TMP_Text livesText;
+
+    public void Start()
+    {
+        moneyText.text = money.ToString();
+        livesText.text = lives.ToString();
+    }
+
     public void updatePoints(int myPoints)
     {
         points += myPoints;
-        //Debug.Log("Points: " + points);
+        money += myPoints * 100;
+        moneyText.text = money.ToString();
     }
     
     public void updateLives(int myLives)
     {
         lives += myLives;
-        //Debug.Log("Lives: " + lives);
+        livesText.text = lives.ToString();
     }
 }
