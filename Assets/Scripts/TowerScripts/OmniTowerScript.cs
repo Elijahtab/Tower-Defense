54906896eb8f6c40bdeb970f1d6bdcbf850a1ffc
Elijahtab <elijah.tabachnik@gmail.com>
2023-10-09 22:19:34 -0700

    s

--- Assets/Scripts/TowerScripts/OmniTowerScript.cs
+++ Assets/Scripts/TowerScripts/OmniTowerScript.cs
@@ -32,21 +32,20 @@ public class OmniTowerScript : MonoBehaviour
     void Update()
     {
         foreach (var obj in enemyEntityManager.Entities)
+            {
+                Vector2 entityPosition = new Vector2(obj.transform.position.x, obj.transform.position.y);
+
+                float distanceToEnemy = Vector2.Distance(entityPosition, transform.position);
+                if(distanceToEnemy <= maxDistance)
                 {
-                    Vector2 entityPosition = new Vector2(obj.transform.position.x, obj.transform.position.y);
-
-                    float distanceToEnemy = Vector2.Distance(entityPosition, transform.position);
-
-                    if(distanceToEnemy <= maxDistance)
-                    {
-                        enemyInRange = true;
-                    }
-                        
+                    enemyInRange = true;
                 }
+                        
+            }
+
         if (delayTimer <= 0f && myTowerPlaced == true && enemyInRange == true)
             {
                 float angleStep = 360.0f / numProjectiles;
-
                 for (int i = 0; i < numProjectiles; i++)
                 {
                     float angle = i * angleStep;
